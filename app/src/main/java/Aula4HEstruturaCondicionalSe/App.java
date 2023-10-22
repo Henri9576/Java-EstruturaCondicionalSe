c0ebccdb4479317e3f366198d84bf9560df77b45
Henrique9576 <henrique_costa02@outlook.com>
2023-10-22 17:30:27 -0300

    Initial commit

---
+++ app/src/main/java/Aula4HEstruturaCondicionalSe/App.java
@@ -0,0 +1,33 @@
+package Aula4HEstruturaCondicionalSe;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int media = 7;
+        
+        /*
+        > maior
+        > maior ou igual
+        < menor
+        <= menor ou igual
+        == igual
+        != diferente
+        
+        && e
+        || ou
+        ! negação
+        */
+        
+        if(media >= 7){
+            if(media == 10) {
+                //aprovado com nota maxima
+            } else {
+                //aprovado
+            }
+        } else {
+            //reprovado
+        }
+        
+    }
+}
